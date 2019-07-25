a0da6f62e0c1573eef5982955316c41d27a5a3ec
sontseslav <s.khvalinsky@gmail.com>
2019-07-25 22:33:50 +0300

    Init

---
+++ src/main/java/com/mardes/kafkaTestServer/config/KafkaConsumerConfig.java
@@ -0,0 +1,22 @@
+package com.mardes.kafkaTestServer.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${kafka.server}")
+    private String kafkaServer;
+
+    @Value("${kafka.group.id}")
+    private String kafkaGroupId;
+
+    @Bean
+    public KafkaListenerContainerFactory<?> batchFactory() {
+        ConcurrentKafkaListenerContainerFactory<Long, AbstractDto> factory;
+    }
+}
